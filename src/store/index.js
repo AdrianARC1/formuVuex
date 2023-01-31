411c65497162434de76bdf2baea7f6bb9c70a58f
Adrián Ramón <adrianramon2003@gmail.com>
2023-01-31 13:38:08 +0100

    Inicializado proyecto form con vuex y creadas vistas

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    nombre: "",
+    edad: "",
+    datos_login:[]
   },
   getters: {
   },
   mutations: {
+    addPersonVuex(state){
+      state.datos_login.push({nombre: state.nombre, edad: state.edad})
+
+      state.nombre=''
+      state.edad=''
+      console.log(state.datos_login)
+    }
   },
   actions: {
+    accionAddPerson(context){
+      context.commit('addPersonVuex')
+    }
   },
   modules: {
   }
---
+++ src/views/VistaFormu.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <label for="">Nombre</label>
+    <input type="text" placeholder="nombre" v-model="$store.state.nombre">
+
+    <label for="">Edad</label>
+    <input type="text" placeholder="nombre" v-model="$store.state.edad">
+    <button @click="addPerson">Enviar</button>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'VistaFormu',
+    methods:{
+        addPerson(){
+            this.$store.dispatch('accionAddPerson')
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
